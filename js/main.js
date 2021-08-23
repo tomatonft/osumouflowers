070ae37bc7b0724ca9887eeb8d93b0f8f453b848
fantasia <87534145+tomatonft@users.noreply.github.com>
2021-08-24 00:04:47 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@
   const open = document.getElementById('open');
   const overlay = document.querySelector('.overlay');
   const close = document.getElementById('close');
+  const links = document.querySelectorAll("div.overlay a");
 
   open.addEventListener('click', () => {
    overlay.classList.add('show');
    open.classList.add('hide');
+
   });
 
   close.addEventListener('click', () => {
@@ -15,5 +17,35 @@
    open.classList.remove('hide');
   });
 
- 
+  document.querySelectorAll('div.overlay a')[0].addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+  document.querySelectorAll('div.overlay a')[1].addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+  document.querySelectorAll('div.overlay a')[2].addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+  document.querySelectorAll('div.overlay a')[3].addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+  document.querySelectorAll('div.overlay a')[4].addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+  document.querySelectorAll('div.overlay a')[5].addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+  document.querySelectorAll('div.overlay a')[6].addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+  
+   
 }
