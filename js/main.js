5ddf2fb77ad0a2c431b373239b851415c1920abe
fantasia <87534145+tomatonft@users.noreply.github.com>
2021-10-04 13:22:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -46,6 +46,28 @@
   //   open.classList.remove('hide');
   // });
 
-  
+  // 交差監視API
+
+  function callback(entries, obs){
+    entries.forEach(entry =>{
+      if (!entry.isIntersecting){
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+  const targets = document.querySelectorAll('.animate');
+
+  targets.forEach(target => {
+    observer.observe(target);
+  });
    
 }
